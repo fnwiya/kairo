f208facabf6f086a9e03a7d80d72781aa0d243aa
fnwiya <fnwiya@gmail.com>
2018-02-27 22:36:06 +0900

    master:urfave/cli sample

--- src/main.go
+++ src/main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+)
+
 func main() {
+	app := cli.NewApp()
+	app.Name = "kairo"
+	app.Usage = "warm up postgresql"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("hello")
+		return nil
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
